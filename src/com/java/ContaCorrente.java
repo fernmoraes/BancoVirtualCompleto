c52fddf87ffba877c432c55009943013e21d173f
Fernando Navajas Moraes <100091001+fernmoraes@users.noreply.github.com>
2025-02-18 21:28:51 -0300

    ContaCorrente.java cheque especial criado

--- src/com/java/ContaCorrente.java
+++ src/com/java/ContaCorrente.java
@@ -2,4 +2,26 @@ package com.java;
 
 public class ContaCorrente extends Conta {
 
+    private String tipo;
+    private Double chequeEspecial;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Double getChequeEspecial() {
+        return chequeEspecial;
+    }
+
+    public void setChequeEspecial(Double chequeEspecial) {
+        this.chequeEspecial = chequeEspecial;
+    }
+
+    public double getSaldoDisponivel() {
+        return super.getSaldo() + chequeEspecial;
+    }
 }
