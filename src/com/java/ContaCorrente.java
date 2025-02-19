f4914ed25fe982f2f75305c406a865b24897cbb3
Fernando Navajas Moraes <100091001+fernmoraes@users.noreply.github.com>
2025-02-18 21:36:57 -0300

    Adicionado construtores na ContaCorrente.java

--- src/com/java/ContaCorrente.java
+++ src/com/java/ContaCorrente.java
@@ -5,6 +5,18 @@ public class ContaCorrente extends Conta {
     private String tipo;
     private Double chequeEspecial;
 
+    // Construtor padrão
+    public ContaCorrente() {
+        super(); // Chama o construtor padrão da classe Conta
+    }
+
+    // Construtor com parâmetros
+    public ContaCorrente(int agencia, int numero, double saldo, String nome, int senha, String tipo, Double chequeEspecial) {
+        super(agencia, numero, saldo, nome, senha); // Chama o construtor da classe Conta
+        this.tipo = tipo;
+        this.chequeEspecial = chequeEspecial;
+    }
+
     public String getTipo() {
         return tipo;
     }
